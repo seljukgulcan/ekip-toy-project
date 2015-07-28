90691b938fb3e9dadcba587b2923beab5c0cbdef
Selçuk Gülcan <selcuk.gulcan@ug.bilkent.edu.tr>
2015-07-28 21:31:00 +0300

    git.cmd change

--- git.cmd
+++ git.cmd
@@ -1,5 +1,5 @@
 SET path=<path>
-%foo%git pull
-%foo%git add -A .
-%foo%git commit -m "change"
-%foo%git push
+%path%git pull
+%path%git add -A .
+%path%git commit -m "default change message"
+%path%git push
